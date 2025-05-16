4014bedf20f44715c32d0c2fba51ec73a6eb8a8a
ornaras <ornaras.us@gmail.com>
2025-05-16 16:33:38 +0400

    Добавление атрибута параметра парсера

---
+++ SourceOptionAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace RetailCorrector
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class SourceOptionAttribute(string name) : Attribute
+    {
+        public string Name { get; set; } = name;
+    }
+}
