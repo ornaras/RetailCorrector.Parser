a87b1549729ddddb010dba95c16311ec53366b34
ornaras <ornaras.us@gmail.com>
2025-05-19 14:10:50 +0400

    Удаление неиспользуемого атрибута

--- SourceOptionAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RetailCorrector
-{
-    [AttributeUsage(AttributeTargets.Property)]
-    public class SourceOptionAttribute(string name) : Attribute
-    {
-        public string Name { get; set; } = name;
-    }
-}
