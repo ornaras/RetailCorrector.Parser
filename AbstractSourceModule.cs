a4e1ce8d970f115ecc0eb22bd3e2be6337f2dc01
ornaras <ornaras.us@gmail.com>
2025-05-29 11:21:15 +0400

    Добавлены события для работы с ProgressBar

--- AbstractSourceModule.cs
+++ AbstractSourceModule.cs
@@ -9,9 +9,13 @@ namespace RetailCorrector
         public event Action<bool, string, Exception?>? OnLog;
         public event PropertyChangedEventHandler? PropertyChanged;
         public event Action<string>? OnNotify;
+        public event Action<int>? ParseStarted;
+        public event Action<int>? ProgressUpdated;
 
         protected internal void Log(bool isError, string message, Exception? exception = null) =>
             OnLog?.Invoke(isError, message, exception);
+        protected internal void OnParseStarted(int maxProgress) => ParseStarted?.Invoke(maxProgress);
+        protected internal void OnProgressUpdated(int value) => ProgressUpdated?.Invoke(value);
         protected internal void Notify(string message) => OnNotify?.Invoke(message);
         protected internal void OnPropertyChanged([CallerMemberName] string propertyName = "") =>
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
